c4eddad5be59fbe5e3812df9d3ebeb180002d4d0
Raphael Turquet <raphael.turquet@bts-malraux.net>
2016-12-02 11:53:14 +0100

    Mise en générique de connexionBDD.php

--- Travis/Projet Raphael/connexionBDD.php
+++ Travis/Projet Raphael/connexionBDD.php
@@ -2,7 +2,7 @@
 // Connexion à la base de donnée CMCTool
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=tracescmc;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=tracescmc;charset=utf8', 'VOTRE IDENTIFIANT', 'VOTRE MOT DE PASSE');
 	$bdd->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
 }
 catch (Exception $e)
@@ -13,7 +13,7 @@ catch (Exception $e)
 // Connexion à la base de donnée traceForum
 try
 {
-	$bdd2 = new PDO('mysql:host=localhost;dbname=traceforum;charset=utf8', 'root', '');
+	$bdd2 = new PDO('mysql:host=localhost;dbname=traceforum;charset=utf8', 'VOTRE IDENTIFIANT', 'VOTRE MOT DE PASSE');
 	$bdd2->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
 }
 catch (Exception $e)
